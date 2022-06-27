14e9affa57ca2244f1a5fd519312f915c4a1c553
Ismael <ismael@doublepanic.com>
2022-06-27 11:37:10 +0200

    Frist post

---
+++ README.md
@@ -0,0 +1,185 @@
+# ArturCON
+
+ArturCON is a quick Python 3 script for using your old Arturia BeatStep as a physical button launch pad for applications, actions, text macros or almost anything that can be made using a keyboard. Just like Stream Deck and the like, but cheaper.
+
+It's capable of:
+* Launch any system shortcut 
+* Multimedia actions (mute, play and pause, volumen up and down, etc)
+* Write strings (so not have to type them every time) 
+* Macros for any app by pressing sequences of keys
+* Send direct keycodes
+
+You can define a pad as push-only or toggl. If you want a pad to launch a program, just assign a key combination (including some control keys) to it at the system shortcut configuration, then add the same combo to the script config. If just want to activate some application feature, just add the key combination to Arturcon.
+
+The script also provide aditional functionalities as:
+* MIDI monitor (for the BeatStep input).
+* MIDI thru, in the case you still need the BeatStep to raw communicate to a plugin.
+* Blinkenlights, for you to amaze the world.
+
+All these functionalities can be mixed with the main launchpad one. And if you launch it as "--resident" it will survive a suspension or even a BeatStep disconnection and back to work when you reconnect it. 
+
+ArturCON uses the pads, so leaves the pots free for you to assign to other applications (Jack plugins, for example)
+
+![alt text](https://github.com/Ismas/arturcon/blob/master/arturcon.png?raw=true)
+
+## How to use
+Configure your desired actions, launch the script with the desired parameters, connect your BeatStep, be happy.
+Read the How to config section at the bottom of this readme.
+
+```bash
+python3 ./arturcon.py --resident --monitor --thru --blinken --process
+```
+You have 16 pads per bank, and 16 banks  of pads, so plenty of combination and posibilities. To change bank just press [CHAN] + [pad number]. Pads are assigned to channels.
+
+Press briefly the [CHAN] button to relit the pads in case the went not correct. The pads are relight every 10 seconds.
+
+
+### Installation
+
+You need the following dependencies:
+* Python 3
+* Mido
+* pynput
+* Scipy
+
+Use your favorite system package manager or the Python one [pip](https://pip.pypa.io/en/stable/) to install them.
+
+```bash
+pip install mido
+pip install pynput
+pip install scipy
+```
+
+### Usage
+
+```bash
+$ python3 ./arturcon.py --help
+ArturCON, the poor man's button deck V0.0.3
+
+Usage: arturcon [parameters]
+
+	Modes (pick one):
+	-p, --process:	Use your BeatStep as a button deck (Default)
+		The folowwing modes can be used themselves or added to 'process'
+	-m, --monitor:	Just print BeatStep MIDI messages at stout
+	-t, --thru:	Just creates a virtual output MIDI port and redirects BeatStep messages (not very practical)
+	-b, --blinken:	Awesome astonising blinkenlights on your BeatStep! 
+
+	Options:
+	-s, --select:	List and select BeatStep MIDI port (autodetected by default)
+	-d, --debug:	Print debug messages at stderr
+	-r, --resident:	Keeps looking for BeatStep, for start-at-boot use and make permanent. Disables -s and -d
+	-v, --version:	Print version and exits
+	-h,?, --help:	This text
+
+```
+
+## How to config
+
+Config is made at the top of the script. You have a dictionary of dictionaries (one for bank) with pad to action assign. Actions are defined as pynput constants. I thing it's pretty autoexplanatory, so just gave it a look. 
+First dictionary is bank 0/channel 0, second is bank 1/channel 1, and so on.
+
+```python
+{ "pad":1, "note": 45, "a":[kk.media_play_pause], "toggle":True, "state":False },
+```
+* Pad: is the pad number, starting at 0 and reading left to right and top to bottom. So "1" is the second.
+* Note: Don't change this, the script will do it for you.
+* a: The actions. It's an array of keys to press, a string or a keychar. You have the possible actions below.
+* toggle: Define if the button is toggable instead of push-only. When toggle, the pad will be lit magenta when "on" and red when "off". For example, to enable or disable the mute:
+```python
+{ "pad":7, "note": 51, "a":[kk.media_volume_mute], "toggle":True, "state":False },   
+```
+* state: Defines the inital state for a toggle pad..
+
+
+So you have:
+* Keys
+
+kk.caps_lock  
+kk.backspace  
+kk.delete  
+kk.enter  
+kk.esc  
+kk.menu  
+kk.num_lock  
+kk.space  
+kk.tab  
+kk.f1 to kk.f12
+
+* Cursors and special 
+
+kk.left  
+kk.right  
+kk.up  
+kk.down  
+kk.insert  
+kk.home  
+kk.end  
+kk.page_up  
+kk.page_down  
+kk.pause  
+kk.print_screen  
+kk.scroll_lock  
+
+* Actions
+
+kk.media_next  
+kk.media_play_pause  
+kk.media_previous  
+kk.media_volume_down  
+kk.media_volume_mute  
+kk.media_volume_up  
+
+* Modifiers:
+
+Modifiers act different to other keys. They keep pressed until you specify them to be realeased by invoking again. 
+For example for perform and [ALT}+[F4] to close the focused application:
+ ```python
+{ "pad":14, "note": 42, "a":[kk.alt,kk.f4,kk.alt], "toggle":False, "state":False },
+````
+
+kk.alt
+kk.alt_gr  
+kk.alt_l  
+kk.alt_r  
+kk.cmd  
+kk.cmd_l  
+kk.cmd_r  
+kk.ctrl  
+kk.ctrl_l  
+kk.ctrl_r  
+kk.shift  
+kk.shift_l  
+kk.shift_r  
+
+And also have:
+* Strings. Just that. You can combine them with the rest of the actions.
+ ```python
+ { "pad":3, "note": 47, "a":[kk.shift,"M",kk.shift,"yUser",kk.tab], "toggle":False, "state":False },  
+```
+* Keycodes
+Some keyboard shortcuts do not work with the key and you should provide the keycode. Use them this way:
+pk.KeyCode(code)
+ ```python
+{ "pad":15, "note": 43, "a":[kk.ctrl_l, kk.alt_l, pk.KeyCode(76),kk.ctrl_l, kk.alt_l], "toggle":False, "state":False }   
+```
+
+# How it works
+
+ArturiCON works by reading BeatStep MIDI input, and asigning note values to actions. For this all pads should be assigned to "General" channel and each pad configured to play one note. You don't have to do this as the script does it for you. AFAIK this will not modify your stored configuration, so just stop Arturicon, recall a config and .
+
+# TODOs
+* Make something with the pots
+* ?
+
+# Bugs
+* Sometimes the first letter of a string is not recognized as uppercase. In that case do [ kk.shift, "r", kk.shift, "est of the string"]
+* Sure, many. Just tell me.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ arturcon.py
@@ -0,0 +1,413 @@
+#!env python3
+############################################
+# ArturCON:
+#   An Arturia BeatStep button launchpad
+############################################
+# Many thanks to Untergeek for the sysex compilation:
+# https://www.untergeek.de/2014/11/taming-arturias-beatstep-sysex-codes-for-programming-via-ipad/
+# Ismas May-June 2022
+# https://doublepanic.com
+# (c) License MIT
+# V0.0.3
+
+#import arturconf
+import sys
+import os
+import random
+import time
+import mido as md
+import getopt
+import threading
+import mido.backends.rtmidi
+from pynput import keyboard as pk
+from pynput.keyboard import Key as kk
+from scipy import rand  
+from time import sleep
+
+##################################################
+# ACTION MAPS
+# EL CANAL midi es un bancoe. 16 canales.
+##################################################
+
+#channel is positional, pad isn't
+#This is it for rendimiento
+pads = [
+    [ #channel/bank 0
+        { "pad":0, "note": 44, "a":[kk.media_previous], "toggle":False, "state":False },   
+        { "pad":1, "note": 45, "a":[kk.media_play_pause], "toggle":True, "state":False },   
+        { "pad":2, "note": 46, "a":[kk.media_next], "toggle":False, "state":False },   
+        { "pad":3, "note": 47, "a":["MyUser",kk.tab], "toggle":False, "state":False },   
+        { "pad":4, "note": 48, "a":[kk.shift,"M",kk.shift,"yPasswd",kk.enter], "toggle":False, "state":False },   
+        { "pad":5, "note": 49, "a":[kk.media_volume_down], "toggle":False, "state":False },   
+        { "pad":6, "note": 50, "a":[kk.media_volume_up], "toggle":False, "state":False },   
+        { "pad":7, "note": 51, "a":[kk.media_volume_mute], "toggle":True, "state":False },   
+        { "pad":8, "note": 36, "a":[kk.shift, kk.tab, kk.shift], "toggle":False, "state":False },   
+        { "pad":9, "note": 37, "a":[kk.alt, kk.tab], "toggle":True, "state":False },   
+        { "pad":10, "note": 38, "a":[kk.tab], "toggle":False, "state":False },   
+        { "pad":11, "note": 39, "a":[kk.alt_l, kk.print_screen,kk.alt_l], "toggle":False, "state":False },   
+        { "pad":12, "note": 40, "a":["FirstField",kk.tab,"SecondField",kk.tab,"ThirdField",kk.enter], "toggle":False, "state":False },   
+        #{ "pad":13, "note": 41, "a":[], "toggle":False, "state":False },   
+        { "pad":14, "note": 42, "a":[kk.alt,kk.f4,kk.alt], "toggle":False, "state":False },   
+        { "pad":15, "note": 43, "a":[kk.ctrl_l, kk.alt_l, pk.KeyCode(76),kk.ctrl_l, kk.alt_l], "toggle":False, "state":False }   
+    ],[ #channel/bank 1
+        { "pad":0, "note": 44, "a":["Your actions here"], "toggle":True, "state":True }   
+    ],[ #channel/bank 2, and so on
+    ],[],[],[],[],[],[],[],[],[],[],[],[],[]
+]
+#####################
+# Don't touch below here
+##################################                                  
+#GLOBALS
+kb      = pk.Controller()
+tport   = ""
+ioport  = ""
+chnl    = 0
+achnl   = -1
+cols    = { "negro": 0x00, "rojo":0x01, "azul":0x10, "magenta":0x11 }
+VER     = "0.0.3"
+DO_PROC = False
+BLINKEN = False
+THRU    = False
+DEBUG   = False
+MONITOR = False
+SELECT  = False
+RESIDENT= False
+CTRLBTN = 0x20
+TAIMER  = 60
+LEDON   = cols["rojo"]
+LEDOFF  = cols["magenta"]
+LEDTOUCH = cols["negro"]
+###################################
+
+#################
+# AUX FUNCTIONS
+#############3
+def debug(cadena):
+    # Mensajes de debug
+    if DEBUG: print("DB: ",cadena, file=sys.stderr)
+
+def error(cadena):
+    # Mensajes de error
+    print("Error: ",cadena, file=sys.stderr)
+    sys.exit(1)
+
+def ledon(pad,color=cols["negro"]):
+    # Change pad color
+    ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x02 ,0x00 ,0x10 ,0x70+pad ,color ]))
+    sleep(.0025)
+    #debug("Color: Pad "+str(pad)+" col "+str(color))
+
+def monitor(cadena):
+    # Imprime los códigos MIDI recibidos
+    if MONITOR: print(str(int(time.time())), cadena, file=sys.stderr)
+
+def select_midin():
+    debug("select_midin:---------")
+    # Menu para que el usuario seleccione un puerto
+    global ioport
+    mdins = md.get_ioport_names()
+    print("\n\nMIDI ports:\n")
+    k=0
+    for i in mdins:
+        k+=1
+        print (k,"-",i)
+    n = -1    
+    while n < 0:
+        try:
+            n = int(input("\nMIDI port [0 exit]: "))
+            if n > k:
+                print("Can't see that")
+                n = -1
+        except:
+            print("Just a number, please")
+            n = -1
+    if (n==0): exit()
+    ioport=md.open_ioport(mdins[n-1])
+
+def keycontrol(lista):
+    # Invoca a las teclas. Pueden ser letras, códigos sueltos, modificadores o cadenas
+    # Totally hate this
+    for tecla in lista:
+        # Si es un modificador hago "touch" para dar la posibilidad de soltarla en la secuencia
+        ltecla = { tecla }
+        if ( kk.alt or kk.alt_l ) in ltecla:
+            kb.touch(tecla, not kb.alt_pressed)
+            continue
+        if kk.alt_gr in ltecla:
+            kb.touch(tecla, not kb.alt_gr_pressed)
+            continue
+        if (kk.ctrl or kk.ctrl_l or kk.ctrl_r) in ltecla:
+            kb.touch(tecla, not kb.ctrl_pressed)
+            continue
+        if (kk.shift or kk.shift_l or kk.shift_r ) in ltecla:
+            kb.touch(tecla, not kb.shift_pressed)
+            continue     
+        # Si es cadena, cadena
+        if type(tecla)== str: 
+            kb.type(tecla)
+        # Si no es ni cadena ni modifier, es código suelto
+        else:
+            kb.tap(tecla)
+
+def pad2nota(pad):
+    # Calcula la nota de un pad
+    return pad+(44 if pad<8 else 28)
+
+def sysend(cmd,pad,val):
+    # Envía un sysex al chisme para configurar
+    # encoders: 0x20 -0x2f
+    # controles: 0x58 - 0x5F
+    # pads: 0x70 - 0x7f
+    ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x02 ,0x00 ,cmd, pad, val ]))
+    sleep(.01)
+
+def sysread(b1,b2):
+    # Envía un sysex al chisme para leer un parámetro.
+    # devuelve el mensaje
+    # encoders: 0x20 -0x2f
+    # controles: 0x58 - 0x5F
+    # pads: 0x70 - 0x7f
+    ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x01 ,0x00 ,b1, b2]))
+    return ioport.receive()
+
+def inictrl():
+    # Inicia los pads de control.
+    # Chan encendido y como nota
+    # Nota chan 0x20
+    for i in range(0,8):
+        sysend(0x01 ,0x58+i ,0x09)    # comportamiento: nota
+        sysend(0x02 ,0x58+i ,0x70)    # canal: 0 
+        sysend(0x03 ,0x58+i ,CTRLBTN)    # nota: CTRLBTN
+        sysend(0x06 ,0x58+i ,0x01)    # tipo: gate
+        sysend(0x10 ,0x58+i ,0x00)    # Luz chan
+    sysend(0x10 ,0x5f ,0x11)    # Luz chan
+    for i in range(0,15):                 # Comportamiento de los encoders
+        sysend(0x06 ,0x20+i ,0x00)        # absoluto
+        sysend(0x04 ,0x20+i ,0x00)        # lowest
+        sysend(0x05 ,0x20+i ,0x7f)        # top
+    
+def monchan():
+    # Monitoriza canal global cada 2 segundos
+    # Receive global midi channel 
+    global chnl
+    m = sysread(0x40,0x06)
+    if m.type=='sysex': 
+        chnl=m.data[9]
+        inipads()
+        debug("monchan: canal -> "+str(chnl))
+
+def inipads(force=False):
+    # Enciende los pads que sean toggle con el on o off
+    # Los reinicia periodicamente
+    global achnl
+    if ioport != "":
+        if chnl != achnl or force:
+            for i in range(15): ledon(i)
+            achnl = chnl
+        for pad in pads[chnl]:
+            if pad["toggle"]:
+                ledon(pad["pad"],LEDON if pad["state"] else LEDOFF)
+        sysend(0x10 ,0x5f ,0x11)    # Luz shift
+    # Timer 
+    threading.Timer(TAIMER,inipads,args=[True]).start()
+
+def autocon_check():
+    global ioport
+    debug("autocon_check:---------")
+    ioport = ""
+    ports= md.get_ioport_names()
+    for port in ports:
+        debug("Port: "+str(port))
+        if "BeatStep:" in port:
+            ioport=md.open_ioport(port, autoreset=True)
+            return True
+    return False
+
+def autocon(recon=False):
+    global ioport
+
+    conn = autocon_check()
+    if not recon and not conn: 
+        error("Not a BeatStep found. None. Try --select for manual MIDI port selection. ")
+        exit()
+    else:
+        while not conn:
+            debug("BeatStep not found. Retrying...")
+            sleep(2)
+            conn = autocon_check()
+
+    debug("Connected to Arturia BeatStep")
+    sleep(2)
+
+def watchdog():
+    # Comprueba si el chisme sigue enchufado y lo reenchufa
+    global ioport
+    debug("watchdog:---------")
+    ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x01 ,0x00 ,0x40, 0x06]))
+    sleep(0.01)
+    m = ioport.poll()
+
+    if isinstance(m, type(None)):  
+        ioport.close()
+        autocon(recon=True) 
+        inictrl()
+        inipads()
+
+###################################
+# Main funcs
+#####################################
+
+def monitorMIDI():
+    global MONITOR
+    debug("monitorMIDI:---------")
+    # Monitoriza los mensajes del canal indicado
+    MONITOR=True
+    for msg in ioport:
+        monitor(msg)
+
+def repeater():
+    debug("repeater:---------")
+    # Ejecuta el MIDI-THRU: Reenvía los mensaje que llegan
+    for msg in ioport:
+        tport.send(msg)
+        monitor(msg)
+
+def procesa():
+# Compara los códigos MIDI con la lista para invocar a las tecals
+    debug("procesa:---------")
+    global chnl
+
+    #for msg in ioport:
+    while True:
+        msg = None
+        count = 0
+        while isinstance(msg, type(None)) and count<10000:
+            sleep(0.001)
+            msg = ioport.poll()
+            count += 1
+
+        if count == 10000: 
+            watchdog()
+            continue
+
+        if msg.type=='note_on' or msg.type=='note_off':
+            if  msg.type=='note_off' and msg.note == CTRLBTN: monchan() # Montiroiza boton cambio canal
+            for pad in pads[chnl]:
+                if (msg.note == pad["note"]): 
+                    keycontrol(pad["a"])    
+                    debug("Match: "+str(pad["note"])+"- Pad "+ str(pad["pad"])+" a: "+str(pad["a"])+" toggle:"+str(pad["toggle"])+" stat:"+str(pad["state"]))
+                    pad["state"] = not pad["state"]
+                    # Espera a que se suelte la tecla
+                    for msg2 in ioport:
+                            if msg2.note == pad["note"] and msg2.type=='note_off': break
+                    if pad["toggle"]:
+                        ledon(pad["pad"],LEDON if pad["state"] else LEDOFF)
+        if THRU: tport.send(msg)
+        monitor(msg)
+
+def blinken(lon=False):
+    debug("blinken:---------")
+    if lon:
+        for i in range(4):
+            for col in cols:
+                for i in range(1,5):
+                    ledon(i+3,cols[col])
+                    ledon(i+11,cols[col])
+                    ledon(4-i,cols[col])
+                    ledon(12-i,cols[col])
+                    sleep(.05)
+                for i in range(5):
+                    ledon(i+3)
+                    ledon(i+11)
+                    ledon(4-i,)
+                    ledon(12-i)
+                    sleep(.025)
+    for i in range(200 if BLINKEN else 50):
+        ledon(random.randint(0,15),cols["rojo"])
+        ledon(random.randint(0,15),cols["azul"])
+        ledon(random.randint(0,15),cols["magenta"])
+        ledon(random.randint(0,15),cols["negro"])
+        ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x02 ,0x00 ,0x10 ,0x59+random.randint(0,8),random.randint(0,4) ]))
+        ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x02 ,0x00 ,0x10 ,0x59+random.randint(0,8),random.randint(0,4) ]))
+        ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x02 ,0x00 ,0x10 ,0x59+random.randint(0,8),random.randint(0,4) ]))
+        ioport.send(md.Message('sysex',data=[0x00, 0x20, 0x6B, 0x7F ,0x42 ,0x02 ,0x00 ,0x10 ,0x59+random.randint(0,8),random.randint(0,4) ]))
+    for i in range(-22,16):
+        ledon(i)        
+
+def usage():
+    print("ArturCON, the poor man's button deck V"+str(VER))
+    print("\nUsage: arturcon [parameters]")
+    print("\n\tModes (pick one):")
+    print("\t-p, --process:\tUse your BeatStep as a button deck (Default)")
+    print("\t\tThe folowwing modes can be used themselves or added to 'process'")
+    print("\t-m, --monitor:\tJust print BeatStep MIDI messages at stout")
+    print("\t-t, --thru:\tJust creates a virtual output MIDI port and redirects BeatStep messages (not very practical)")
+    print("\t-b, --blinken:\tAwesome astonising blinkenlights on your BeatStep! ")
+    print("\n\tOptions:")
+    print("\t-s, --select:\tList and select BeatStep MIDI port (autodetected by default)")
+    print("\t-d, --debug:\tPrint debug messages at stderr")
+    print("\t-r, --resident:\tKeeps looking for BeatStep, for start-at-boot use and make permanent. Disables -s and -d")
+    print("\t-v, --version:\tPrint version and exits")
+    print("\t-h,?, --help:\tThis text\n")
+
+##################################333   
+# main
+####################################
+
+def main(argv):
+    global tport, DEBUG, DO_PROC, THRU, MONITOR, BLINKEN, SELECT, RESIDENT
+
+    # Read arguments
+    try:
+        opts, args = getopt.getopt(argv,"pmtbdsvrh?",["process","monitor","thru","blinken","select","debug","version","help","resident"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    # Order is importante
+    for opt, arg in opts:
+        if opt in ("-h","-?","--help"): 
+            usage()
+            sys.exit(0)
+        if opt in ("-v","--version"): 
+            print("V:"+VER)
+            sys.exit(0)
+        if opt in ("-d","--debug"):     DEBUG = True
+        if opt in ("-p","--process"):   DO_PROC = True 
+        if opt in ("-t","--thru"):      THRU = True  
+        if opt in ("-m","--monitor"):   MONITOR = True  
+        if opt in ("-b","--blinken"):   BLINKEN = True
+        if opt in ("-s","--select"):    SELECT = True
+        if opt in ("-r","--resident"):  
+            RESIDENT = True
+            SELECT   = False
+            DEBUG    = False
+
+    # If thru selected open port
+    if THRU: tport = md.open_output("ArturiCON_0",True)
+
+    # Use choosen de detection
+    select_midin() if SELECT else autocon(RESIDENT)
+
+    # Greeting
+    print("Arturcon at your service!")
+
+#    try:
+    if not (MONITOR or BLINKEN or THRU) or DO_PROC:
+        blinken(BLINKEN)
+        inictrl()
+        monchan()
+        inipads()
+        procesa() 
+    if BLINKEN: blinken(True)
+    if MONITOR: monitorMIDI()
+    if THRU:    repeater()
+    debug("END-----------")
+#    except:
+#        main(sys.argv[1:])
+
+if __name__ == "__main__":
+    # DETACH!
+    #if os.fork(): sys.exit()
+    # RRUN!
+    main(sys.argv[1:])
